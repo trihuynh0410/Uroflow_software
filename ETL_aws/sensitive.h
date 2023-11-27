7dfa62c8a417b82d92b4cb15c486690979504cab
trihuynh0410 <trihuynh0410@gmail.com>
2023-11-27 16:54:28 +0700

    Remove sensitive.h from tracking

--- ETL_aws/sensitive.h
+++
@@ -1,7 +0,0 @@
-// secrets.h
-#define WIFI_SSID "Tris"
-#define WIFI_PASSWORD "04102002"
-#define AWS_S3_BUCKET "uro-rawzone/"
-#define AWS_REGION "ap-southeast-1" // e.g., us-west-2
-#define API_URL "https://bj7aheyi21.execute-api.ap-southeast-1.amazonaws.com/dev/" 
-#define FILE_PATH "/" // Path in SPIFFS
